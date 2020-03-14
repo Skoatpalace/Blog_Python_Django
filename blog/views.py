49a31af82fca26198b27657d967e583c1c38f999
Skoatpalace <richard.fages@gmail.com>
2020-03-14 09:47:02 +0100

    add llist categories

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,13 @@ from blog.models import Post
 from blog import model_helpers
 
 
-
 def post_list(request, category_name=model_helpers.post_category_all.slug()):
     category, posts = model_helpers.get_category_and_posts(category_name)
-
+    categories = model_helpers.get_categories()
     context = {
         'posts': posts,
         'category': category,
+        'categories': categories,
     }
     return render(request, 'blog/post_list.html', context)
+
